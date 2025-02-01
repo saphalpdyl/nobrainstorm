e0608eaddc27b3294090baad76485f82290bfe78
saphalpdyl <saphalpdyl@gmail.com>
2025-02-01 14:55:46 -0500

    remove: some components

--- src/components/user-canvas.tsx
+++ src/components/user-canvas.tsx
@@ -9,27 +9,15 @@ export default function UserCanvas() {
   return (
     <div className="w-full h-full">
       <Tldraw
-        shapeUtils={[ComplexityTimePlot]}
-        onMount={(editor) => {
-          // Mock data for time complexity plot and add shape
-            const data = Array.from({ length: 5 }, (_, i) => ({
-              name: String.fromCharCode(65 + i),
-              complexity: Math.random(),
-              time: Math.random(),
-            }));
-
-          editor.createShape({
-            type: "complexity-time-plot",
-            x: 100,
-            y: 100,
-            props: {
-              w: 600,
-              h: 400,
-              data,
-            },
-          });
-          
+        components={{
+          MainMenu: () => null,
+          PageMenu: () => null,
+          ActionsMenu: () => null,
+          ZoomMenu: () => null,
+          Minimap: () => null,
+          QuickActions: () => null,
         }}
+        shapeUtils={[ComplexityTimePlot]}
       >
         
       </Tldraw>
