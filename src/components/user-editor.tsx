acbe3cdb09acb0e7ab4e4b6e827289dc38a13b69
saphalpdyl <saphalpdyl@gmail.com>
2025-02-02 02:30:41 -0500

    fix: shape selection a little better

--- src/components/user-editor.tsx
+++ src/components/user-editor.tsx
@@ -10,17 +10,19 @@ export default function UserEditor() {
     const handlePointerMove = (e: TLEventInfo) => {
       if ( e.name === "pointer_down" ) {
         // Loop through all the text object in the editor
-        const shapesAtPoint = editor.getShapesAtPoint({
-          x: e.point.x,
-          y: e.point.y,
-        });
+        // const shapesAtPoint = editor.getShapesAtPoint({
+        //   x: e.point.x,
+        //   y: e.point.y,
+        // });
+
+        const shapesAtPoint = editor.getSelectedShapes();
+
 
         // find from back of the list
         let hoveredTextShape: TLShape | null = null;
 
         for ( let i = shapesAtPoint.length - 1; i >= 0; i-- ) {
           if ( shapesAtPoint[i].type === "text" || shapesAtPoint[i].type === "geo" ) {
-            console.log(hoveredTextShape)
             hoveredTextShape = shapesAtPoint[i];
             break;
           }
