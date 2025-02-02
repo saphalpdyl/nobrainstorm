d8f476f57a0defe46686fdc60826a7f2480f1b4d
Sugam Panthi <man359905@gmail.com>
2025-02-01 20:23:33 -0600

    refactor

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,3 @@
-// Two parameters with 0 to 1 range, the more it is the greener it is and the less it is the redder it is, in hex.
-export const getComplexityColor = (complexity: number, time: number) => {
-  const complexityColor = Math.round(complexity * 255).toString(16).padStart(2, '0');
-  const timeColor = Math.round(time * 255).toString(16).padStart(2, '0');
-  return `#${timeColor}${complexityColor}00`;
-}
-
 export function cleanMarkdown(markdown: string): string {
   // Remove bold and italic markers
   let cleaned = markdown.replace(/(\*\*|__)(.*?)\1/g, '$2')  // Bold
